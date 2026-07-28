63c822fe8e107a2e8e23a5aa866e5f19455afa66
min123dy <dyleo1004@gmail.com>
2026-07-29 04:41:41 +0900

    Update py_build_dll.py

--- py_build_dll.py
+++ py_build_dll.py
@@ -1,5 +1,10 @@
 # Copyright (c) 2026 min123dy
-# MPL-2.0 notice
+#
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import os
 import sys
 import shutil
